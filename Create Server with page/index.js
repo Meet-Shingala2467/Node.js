90b8088b439168679a4a43e759654f0355c7ee84
Meet Shingala2467 <meetshingala5557@gmail.com>
2024-12-23 18:25:41 +0530

    12/23/2024

--- Create Server with page/index.js
+++ Create Server with page/index.js
@@ -3,38 +3,37 @@ const port = 9000;
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-
-  let fileName="";
-    switch(req.url){
-        case "/":
-            fileName = "./home.html";
-        break;
-        case "/about":
-            fileName = "./About.html";
-        break;
-        case "/contact":
-            fileName = "./Contact.html";
-        break;
-        case "/blog":
-            fileName = "./Blog.html";
-        break;
-        case "/shop":
-            fileName = "./Shop.html";
-        break;
-    }
-});
-
-fs.readFile(fileName,(err,pagename)=>{
-  if(err){
-    console.log('file is not found');
-    return false;
+  let fileName = "";
+  switch (req.url) {
+    case "/":
+      fileName = "./home.html";
+      break;
+    case "/about":
+      fileName = "./About.html";
+      break;
+    case "/contact":
+      fileName = "./Contact.html";
+      break;
+    case "/blog":
+      fileName = "./Blog.html";
+      break;
+    case "/shop":
+      fileName = "./Shop.html";
+      break;
+ 
   }
-  res.end(pagename);
-})
 
+  fs.readFile(fileName, (err, pagename) => {
+    if (err) {
+      return false;
+    }
+  
+    res.end(pagename);
+  });
+});
 
 server.listen(port, (err) => {
   if (!err) {
-    console.log(`Server is run On:-${port}`);
+    console.log(`Server is running on: ${port}`);
   }
 });
