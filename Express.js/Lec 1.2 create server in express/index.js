375fd7961b6691397a184ef3a66269ffa67ad308
Meet Shingala2467 <meetshingala5557@gmail.com>
2024-12-25 10:51:24 +0530

    12/25/2024

---
+++ Express.js/Lec 1.2 create server in express/index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+
+const port = 9000;
+
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+    return res.render('Home');
+    // res.end();
+});
+
+app.get('/contact', (req, res) => {
+    return res.render('Contact');
+    // res.end();
+});
+
+app.get('/product', (req, res) => {
+    return res.render('Product');
+    // res.end();
+});
+
+app.listen(port, (err) => {
+    if (err) {
+        console.log(err);
+        return false;
+    }
+    console.log(`Server is running on http://localhost:${port}`);
+});
