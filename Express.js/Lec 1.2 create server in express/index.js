ea83447d1b027c87b3dd22be4b9b57b82187fe31
Meet Shingala2467 <meetshingala5557@gmail.com>
2025-01-03 13:40:47 +0530

    d

--- Express.js/Lec 1.2 create server in express/index.js
+++ Express.js/Lec 1.2 create server in express/index.js
@@ -4,27 +4,28 @@ const port = 9000;
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs"); //to assign the setting name to value 
+                              //view engine enables the server-side rendering of dynamic pages
 
-app.get('/', (req, res) => {
-    return res.render('Home');
-    // res.end();
+app.get("/", (req, res) => {
+  return res.render("Home");
+  // res.end();
 });
 
-app.get('/contact', (req, res) => {
-    return res.render('Contact');
-    // res.end();
+app.get("/contact", (req, res) => {
+  return res.render("Contact");
+  // res.end();
 });
 
-app.get('/product', (req, res) => {
-    return res.render('Product');
-    // res.end();
+app.get("/product", (req, res) => {
+  return res.render("Product");
+  // res.end();
 });
 
 app.listen(port, (err) => {
-    if (err) {
-        console.log(err);
-        return false;
-    }
-    console.log(`Server is running on http://localhost:${port}`);
+  if (err) {
+    console.log(err);
+    return false;
+  }
+  console.log(`Server is running on http://localhost:${port}`);
 });
