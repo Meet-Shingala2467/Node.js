873ca7d0ec6982a8a3a6579759720e9d37a85f65
Meet Shingala2467 <meetshingala5557@gmail.com>
2025-01-21 13:48:36 +0530

    21/1

--- Express.js/Crud with mongodb/index.js
+++ Express.js/Crud with mongodb/index.js
@@ -1,28 +1,38 @@
 const express = require('express');
-const users = require('./models/userModel');
+const userModel = require('./models/userModel');
 const port = 9000;
 const app = express();
+const path=require('path');
+app.set('view engine','ejs');
+const db=require('./config/db');
 
 app.use(express.urlencoded());
 
+app.get('/',(req,res)=>{
+    return res.render('form');
+})
+
 app.post('/adduser',(req,res)=>{
-    console.log(req.body);
-})
+    const {name,email,password}=req.body;
+    userModel.create({
+        name : name,
+        email : email,
+        password: password,
+    })
+    then((r)=>{
 
-userModel.create({
-    username : name,
-    useremail : email,
-    userpassword: password,
-})
-then((r)=>{
-
-console.log("user create");
-    return res.redirect('/');
-})
+        console.log("user create");
+            return res.redirect('/');
+        })
+        
 .catch((err)=>{
     console.log(err);
     return false;
 })
+
+})
+
+
 
 app.listen(port, (err)=>{
     if(err){
